c5bcdac1aea1684efedf543dc9578eb5f531613a
andres <andres.carrillo@appliedmedical.com>
2025-04-21 00:48:07 -0700

    Class for linking variables to shaders

--- core/attribute.py
+++ core/attribute.py
@@ -1,13 +1,14 @@
-import OpenGL as gl
+from OpenGL.GL import *
+import glfw
 import numpy as np
 
 
 class Attribute:
     def __init__(self,datatype,data):
-        self.datatype = datatype
+        self.data_type = datatype
         self.data = data
 
-        self.bufferRef = gl.glGenBuffers(1)
+        self.bufferRef = glGenBuffers(1)
 
         self.uploadData()
 
@@ -15,32 +16,40 @@ class Attribute:
     # this method should be called after the buffer is bound
     # in the OpenGL context
     def uploadData(self):
-        data = np.array(self.data).astype(np.float32)
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER,data.ravel(),gl.GL_STATIC_DRAW)
+        data = np.array(self.data,dtype=np.float32)
+        glBindBuffer(GL_ARRAY_BUFFER,self.bufferRef)
+
+        # allocate memory for the buffer
+        glBufferData(GL_ARRAY_BUFFER,data.nbytes,data,GL_STATIC_DRAW)
+        # check for errors
+        error = glGetError()
+        if error != GL_NO_ERROR:
+            raise RuntimeError(f"Error uploading data to GPU: {error}")
 
     #associate the buffer with the shader program
-    def associatVariable(self,program_ref:int,variable_name:str) -> None:
+    def associateVariable(self,program_ref:int,variable_name:str) -> None:
+        print("variable name",variable_name)
         # create a reference to a variable with the given name
-        variable_ref = gl.glGetAttribLocation(program_ref,variable_name)
+        variable_ref = glGetAttribLocation(program_ref,variable_name)
 
         # check that the variable is valid
         if variable_ref == -1:
             raise RuntimeError(f"Variable {variable_name} not found in shader program")
         
-        gl.glBindBuffer(gl.GL_ARRAY_BUFFER,self.bufferRef)
+        glBindBuffer(GL_ARRAY_BUFFER,self.bufferRef)
 
-        if self.dataType == "int":
-            gl.glVertexAttribIPointer(variable_ref,1,gl.GL_INT,0,0)
-        elif self.dataType == "float":
-            gl.glVertexAttribPointer(variable_ref,1,gl.GL_FLOAT,gl.GL_FALSE,0,0)
-        elif self.dataType == "vec2":
-            gl.glVertexAttribPointer(variable_ref,2,gl.GL_FLOAT,gl.GL_FALSE,0,0)
-        elif self.dataType == "vec3":
-            gl.glVertexAttribPointer(variable_ref,3,gl.GL_FLOAT,gl.GL_FALSE,0,0)
-        elif self.dataType == "vec4":
-            gl.glVertexAttribPointer(variable_ref,4,gl.GL_FLOAT,gl.GL_FALSE,0,0)
+        if self.data_type == "int":
+            glVertexAttribIPointer(variable_ref,1,GL_INT,False,0,None)
+        elif self.data_type == "float":
+            glVertexAttribPointer(variable_ref,1,GL_FLOAT,False,0,None)
+        elif self.data_type == "vec2":
+            glVertexAttribPointer(variable_ref,2,GL_FLOAT,False,0,None)
+        elif self.data_type == "vec3":
+            glVertexAttribPointer(variable_ref,3,GL_FLOAT,False,0,None)
+        elif self.data_type == "vec4":
+            glVertexAttribPointer(variable_ref,4,GL_FLOAT,False,0,None)
         else:
-            raise RuntimeError(f"Unknown data type {self.dataType}")
+            raise RuntimeError(f"Unknown data type {self.data_type}")
         
         # enable the variable
-        gl.glEnableVertexAttribArray(variable_ref)
+        glEnableVertexAttribArray(variable_ref)
