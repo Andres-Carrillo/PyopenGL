7d86268d8506ff16f23a8a412ff55fb1e3303469
andres <andres.carrillo@appliedmedical.com>
2025-04-24 01:14:47 -0700

    start of group class

---
+++ core/group.py
@@ -0,0 +1,8 @@
+from core.object3D import Object3D
+
+class Group(Object3D):
+    """Group class meant to be a node in the scene graph"""
+    def __init__(self):
+        super().__init__()  
+
+        
