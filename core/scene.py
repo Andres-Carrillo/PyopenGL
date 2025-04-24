5a262591cae287fb4ce4f1d806531716aab20689
andres <andres.carrillo@appliedmedical.com>
2025-04-24 01:15:31 -0700

    start of scene for root node of scene graph

---
+++ core/scene.py
@@ -0,0 +1,9 @@
+from core.object3D import Object3D
+
+class Scene(Object3D):
+    """"Scene class meant to be the root of the scene graph"""
+    def __init__(self):
+        super().__init__()
+
+
+        
