88ea2a43b886940a4eb6f1ec743264be73c58fe5
andres <andres.carrillo@appliedmedical.com>
2025-04-25 20:21:30 -0700

    code update

--- geometry/sphere.py
+++ geometry/sphere.py
@@ -4,12 +4,5 @@ from math import pi, sin, cos
 class Sphere(Ellipsoid):
 
     def __init__(self,radius:float = 1.0,seg_radius:float=32.0,seg_height:float=16) -> None:
-        
-        def parametric_equation(u:float,v:float) -> tuple[float,float,float]:
-            x = radius * sin(u) * cos(v)
-            y = radius * sin(v)
-            z = radius * cos(u) * cos(v)
 
-            return (x,y,z)
-
-        super().__init__(radius,radius,radius,seg_radius,seg_height,parametric_equation)
+        super().__init__(2*radius,2*radius,2*radius,seg_radius,seg_height)
