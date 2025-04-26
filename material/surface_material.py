d2cd9fc7daaca748947406d4c10dd8ed3339612c
andres <andres.carrillo@appliedmedical.com>
2025-04-26 02:21:31 -0700

    surface material

---
+++ material/surface_material.py
@@ -0,0 +1,25 @@
+from material.basic_material import BasicMaterial
+import OpenGL.GL as gl
+
+class SurfaceMaterial(BasicMaterial):
+    def __init__(self,properties:dict = {}) -> None:
+        super().__init__()
+
+        self.settings["draw_mode"] = gl.GL_TRIANGLES
+        self.settings["double_sided"] = False
+        self.settings["wire_frame"] = False
+        self.settings['line_width'] = 1.0
+
+        self.set_properties(properties)
+
+
+    def update_render_settings(self) -> None:
+        if self.settings["double_sided"]:
+            gl.glDisable(gl.GL_CULL_FACE)
+        else:
+            gl.glEnable(gl.GL_CULL_FACE)
+
+        if self.settings["wire_frame"]:
+            gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_LINE)
+        else:
+            gl.glPolygonMode(gl.GL_FRONT_AND_BACK, gl.GL_FILL)
