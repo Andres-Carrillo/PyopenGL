05587aabaa92b2cb6ab4cbbb096410ed5150447f
andres <andres.carrillo@appliedmedical.com>
2025-04-25 22:02:10 -0700

    base class for all cylindrical objects

---
+++ geometry/cylindrical.py
@@ -0,0 +1,37 @@
+from geometry.parametric import Parametric
+from geometry.polygon import Polygon
+from core.matrix import Matrix
+from math import pi, sin, cos
+
+class Cylindrical(Parametric):
+    def __init__(self,top_radius:float = 1.0,bot_radius:float = 1.0,height:float = 1.0,
+                 seg_radius:float=32.0,seg_height:float=4,top_closed=True,bot_closed=True) -> None:
+        
+        def parametric_equation(u:float,v:float):
+            x = (v*top_radius + (1-v)*bot_radius) * sin(u)
+            y = height*(v-0.5)
+            z = (v*top_radius + (1-v)*bot_radius) * cos(u)
+            return (x,y,z)
+        
+        super().__init__(0,2*pi,seg_radius,0,1,seg_height,parametric_equation)
+
+        if top_closed:
+            #create a polygon for the top that matches the shape of the cylinder
+            top_geometry = Polygon(radius=top_radius, sides=seg_radius)
+
+            #transformation to position the polygon at the top of the cylindrical
+            transform = Matrix.make_translation(0, height/2, 0) @ Matrix.make_rotate_y(-pi/2) @ Matrix.make_rotate_x(-pi/2)
+            top_geometry.applyTransform(transform)
+
+            self.merge(top_geometry)
+
+        if bot_closed:
+            bot_geometry = Polygon(radius=bot_radius, sides=seg_radius)
+
+            transform = Matrix.make_translation(0, -height/2, 0) @ Matrix.make_rotate_y(-pi/2) @ Matrix.make_rotate_x(-pi/2)
+            bot_geometry.applyTransform(transform)
+
+            self.merge(bot_geometry)
+
+            
+        
