65c5f7909a675292b4e90e09155cc3ccc5f73617
andres <andres.carrillo@appliedmedical.com>
2025-04-25 22:03:10 -0700

    added functions for merging geometries and applying transformations

--- geometry/geometry.py
+++ geometry/geometry.py
@@ -14,3 +14,31 @@ class Geometry(object):
     def countVertices(self):
         attribute = list(self.attributes.values())[0]
         self.vertex_count = len(attribute.data)
+
+
+    def applyTransform(self,matrix,var_name:str = "vertex_position"):
+        old_position = self.attributes[var_name].data
+        new_position = []
+
+        for prev_pos in old_position:
+            new_pos = prev_pos.copy()
+
+            new_pos.append(1)
+
+            new_pos = matrix @ new_pos
+
+            new_pos = list(new_pos[:3])
+
+            new_position.append(new_pos)
+
+        self.attributes[var_name].data = new_position
+        self.attributes[var_name].uploadData()
+
+
+    def merge(self,other_geometry):
+        for var_name, attrib_obj in self.attributes.items():
+            attrib_obj.data += other_geometry.attributes[var_name].data
+            attrib_obj.uploadData()
+        
+        self.countVertices()
+           
