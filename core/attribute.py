a48bb7192ce148bb43a6cab05c7c7d3beac73592
andres <andres.carrillo@appliedmedical.com>
2025-04-27 00:49:49 -0700

    code  cleanup

--- core/attribute.py
+++ core/attribute.py
@@ -25,30 +25,31 @@ class Attribute:
         error = glGetError()
         if error != GL_NO_ERROR:
             raise RuntimeError(f"Error uploading data to GPU: {error}")
+        
 
     #associate the buffer with the shader program
     def associateVariable(self,program_ref:int,variable_name:str) -> None:
         # create a reference to a variable with the given name
-        variable_ref = glGetAttribLocation(program_ref,variable_name)
+        self.variable_ref = glGetAttribLocation(program_ref,variable_name)
 
         # check that the variable is valid
-        if variable_ref == -1:
-            raise RuntimeError(f"Variable {variable_name} not found in shader program")
+        if self.variable_ref == -1:
+            raise RuntimeError("Variable ", variable_name, "not found in shader program")
         
         glBindBuffer(GL_ARRAY_BUFFER,self.bufferRef)
 
         if self.data_type == "int":
-            glVertexAttribIPointer(variable_ref,1,GL_INT,False,0,None)
+            glVertexAttribIPointer(self.variable_ref,1,GL_INT,False,0,None)
         elif self.data_type == "float":
-            glVertexAttribPointer(variable_ref,1,GL_FLOAT,False,0,None)
+            glVertexAttribPointer(self.variable_ref,1,GL_FLOAT,False,0,None)
         elif self.data_type == "vec2":
-            glVertexAttribPointer(variable_ref,2,GL_FLOAT,False,0,None)
+            glVertexAttribPointer(self.variable_ref,2,GL_FLOAT,False,0,None)
         elif self.data_type == "vec3":
-            glVertexAttribPointer(variable_ref,3,GL_FLOAT,False,0,None)
+            glVertexAttribPointer(self.variable_ref,3,GL_FLOAT,False,0,None)
         elif self.data_type == "vec4":
-            glVertexAttribPointer(variable_ref,4,GL_FLOAT,False,0,None)
+            glVertexAttribPointer(self.variable_ref,4,GL_FLOAT,False,0,None)
         else:
             raise RuntimeError(f"Unknown data type {self.data_type}")
         
         # enable the variable
-        glEnableVertexAttribArray(variable_ref)
+        glEnableVertexAttribArray(self.variable_ref)
