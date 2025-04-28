3876f0d7eb7494c8a9ddd166ebc548e5a2d9f29d
andres <andres.carrillo@appliedmedical.com>
2025-04-28 01:41:30 -0700

    removed deprecated code

--- material/point_material.py
+++ material/point_material.py
@@ -3,11 +3,11 @@ import OpenGL.GL as gl
 
 class PointMaterial(BasicMaterial):
     def __init__(self,properties:dict = {}) ->None:
-        super.__init__()
+        super().__init__()
 
         self.settings["draw_mode"] = gl.GL_POINTS
         self.settings["point_size"] = 8.0
-        self.settings["rounded_points"] = False 
+        # self.settings["rounded_points"] = False 
 
 
         self.set_properties(properties)
@@ -16,7 +16,4 @@ class PointMaterial(BasicMaterial):
     def update_render_settings(self)->None:
         gl.glPointSize(self.settings["point_size"])
 
-        if self.settings["rounded_points"]:
-             gl.glEnable(gl.GL_POINT_SMOOTH)
-        else:
-             gl.glDisable(gl.GL_POINT_SMOOTH)
+             
