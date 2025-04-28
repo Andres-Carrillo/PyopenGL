fe73e2619f453d2f4c234f3e6aa809127fa9a83c
andres <andres.carrillo@appliedmedical.com>
2025-04-28 01:43:58 -0700

    implemented check to make sure the correct shader code is compiled

--- material/basic_material.py
+++ material/basic_material.py
@@ -3,39 +3,43 @@ from core.uniform import Uniform
 import OpenGL.GL as gl
 class BasicMaterial(Material):
 
-    def __init__(self):
-        vertex_shader_code = """
-                            uniform mat4 projection_matrix;
-                            uniform mat4 view_matrix;
-                            uniform mat4 model_matrix;
-                            in vec3 vertex_position;
-                            in vec3 vertex_color;
-                            out vec3 color;
+    def __init__(self,vertex_shader_code = None, fragment_shader_code = None,use_vertex_colors = True) -> None:
+        if vertex_shader_code is None:
+            vertex_shader_code = """
+                                uniform mat4 projection_matrix;
+                                uniform mat4 view_matrix;
+                                uniform mat4 model_matrix;
+                                in vec3 vertex_position;
+                                in vec3 vertex_color;
+                                out vec3 color;
 
-                            void main(){
-                                gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertex_position,1.0);
-                                color = vertex_color;
-                            }
-                            """
-        
-        fragment_shader_code = """
-                            uniform vec3 base_color;
-                            uniform bool use_vertex_colors;
-                            in vec3 color;
-                            out vec4 frag_color;
-                            
-                            void main(){
-                                vec4 temp_color = vec4(base_color,1.0);
-                                
-                                if (use_vertex_colors){
-                                    temp_color = vec4(color,1.0);
+                                void main(){
+                                    gl_Position = projection_matrix * view_matrix * model_matrix * vec4(vertex_position,1.0);
+                                    color = vertex_color;
                                 }
+                                """
+        if fragment_shader_code is None:
+            fragment_shader_code = """
+                                uniform vec3 base_color;
+                                uniform bool use_vertex_colors;
+                                in vec3 color;
+                                out vec4 frag_color;
+                                
+                                void main(){
+                                    vec4 temp_color = vec4(base_color,1.0);
+                                    
+                                    if (use_vertex_colors){
+                                        temp_color = vec4(color,1.0);
+                                    }
 
-                                frag_color = temp_color;
-                            }
-            """
+                                    frag_color = temp_color;
+                                }
+                """
         
+        print("vertex shader code in basic material class: ", vertex_shader_code)
+        print("fragment shader code in basic material class: ", fragment_shader_code)
         super().__init__(vertex_shader_code, fragment_shader_code)
         self.add_uniform("base_color", (1.0, 0.0, 0.0), "vec3")
-        self.add_uniform("use_vertex_colors", False, "bool")
+        if use_vertex_colors:
+            self.add_uniform("use_vertex_colors", use_vertex_colors, "bool")
         self.locate_uniforms()
