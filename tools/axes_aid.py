74e2f37d4ad60a25e895ea7e1fb564598ed40628
andres <andres.carrillo@appliedmedical.com>
2025-04-28 23:04:43 -0700

    helper class for displaying the axis

---
+++ tools/axes_aid.py
@@ -0,0 +1,28 @@
+from core.mesh import Mesh
+from geometry.geometry import Geometry
+from material.line_material import LineMaterial
+
+class AxesAid(Mesh):
+    def __init__(self,axis_len:int =1,line_width =4,axix_colors=[[1,0,0],[0,1,0],[0,0,1]]):
+        geo = Geometry()
+
+        position_data = [[0,0,0],[axis_len,0,0],
+                         [0,0,0],[0,axis_len,0],
+                         [0,0,0],[0,0,axis_len]]
+        
+        color_data = [axix_colors[0],axix_colors[0],
+                        axix_colors[1],axix_colors[1],
+                        axix_colors[2],axix_colors[2]]
+        
+        geo.addAttribute("vertex_position",position_data,"vec3")
+        geo.addAttribute("vertex_color",color_data,"vec3")
+
+        geo.countVertices()
+
+        mat = LineMaterial({
+            "use_vertex_colors":True,
+            "line_width":line_width,
+            "line_type":"segments"
+        })
+
+        super().__init__(geo,mat)
