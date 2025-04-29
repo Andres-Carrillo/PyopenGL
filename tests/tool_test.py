c40e5e1efa1d07205f9fff10c479b0381b0278cd
andres <andres.carrillo@appliedmedical.com>
2025-04-28 23:06:12 -0700

    Test script for grid tool axes helper and movement rig

---
+++ tests/tool_test.py
@@ -0,0 +1,44 @@
+import pathlib
+import sys
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+from tools.axes_aid import AxesAid
+from tools.grid import GridTool
+from tools.movement_rig import MovementRig
+from math import pi
+from tests.template import Test
+
+class ToolTest(Test):
+    def __init__(self):
+        super().__init__(title="Tool Test")
+        
+        self.rig = MovementRig()
+        self.rig.add(self.camera)
+        self.rig.set_pos([0.5, 1, 5])
+
+        self.scene.add(self.rig)
+
+        axes = AxesAid(axis_len=2)
+
+        self.scene.add(axes)
+
+        grid = GridTool(size = 10,division=20,grid_color=[1,1,1],center_color=[1,1,0])
+
+        grid.rotate_x(-pi/2)
+        self.scene.add(grid)
+
+
+    def update(self):
+        self.rig.update(self.input_handler,self.timer.delta_time())
+        super().update()
+
+
+if __name__ == "__main__":
+    test = ToolTest()
+    test.run()
