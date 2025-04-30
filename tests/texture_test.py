2606d2a21bae66ffcdf2d7addc7e0b2e1d7ad68d
andres <andres.carrillo@appliedmedical.com>
2025-04-30 00:55:29 -0700

    testing base texture class

---
+++ tests/texture_test.py
@@ -0,0 +1,45 @@
+import pathlib
+import sys
+
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+from tests.template import Test
+from core.texture import Texture
+from material.texture_material import TextureMaterial
+from geometry.rectangle_geometry import  RectangleGeo
+from geometry.box import BoxGeometry
+from geometry.sphere import Sphere
+from core.mesh import Mesh  
+
+class TextureTest(Test):
+    def __init__(self):
+        super().__init__(title="Texture Test")
+        self.camera.set_pos([0, 0, 2])
+
+        # geometry = RectangleGeo()
+        # geometry = BoxGeometry()
+        geometry = Sphere(radius=0.5, seg_radius=128, seg_height=64)
+        grid = Texture("images/lunar.jpeg")
+        material = TextureMaterial(texture=grid)
+
+        self.mesh = Mesh(geometry, material)
+
+        self.scene.add(self.mesh)
+
+      
+    def update(self):
+        self.mesh.rotate_y(0.00514)
+        self.mesh.rotate_x(0.00337)
+        # Update the scene with the texture applied
+        self.renderer.render(self.scene, self.camera)
+
+
+if __name__ == "__main__":
+    test = TextureTest()
+    test.run()
+    test.quit()
