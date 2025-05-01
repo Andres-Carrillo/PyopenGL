10c0ba87c3048f550c74a8c7169fa4f4ba92118e
andres <andres.carrillo@appliedmedical.com>
2025-04-30 23:44:16 -0700

    improvements to base test class

--- tests/template.py
+++ tests/template.py
@@ -18,27 +18,33 @@ from tools.movement_rig import MovementRig
 
 class Test(Base):
 
-    def __init__(self,clear_color=[0.2, 0.2, 0.2], title="Test"):
+    def __init__(self,clear_color=[0.2, 0.2, 0.2], title="Test",display_grid=True,static_camera=False):
+
         super().__init__(title=title)
         self.renderer = Renderer(clear_color=clear_color)
         self.scene = Scene()
         self.camera = Camera(aspect_ratio=800/600)
         self.camera.set_pos([0, 0, 4])
-
-        self.rig = MovementRig()
-        self.rig.add(self.camera)
-        self.rig.set_pos([0.5, 1, 5])
-
-        self.scene.add(self.rig)
+        self.rig = None
 
 
-        grid = GridTool(size = self.camera.far,division=self.camera.far,grid_color=[1,1,1],center_color=[1,1,0])
+        if not static_camera:
+            self.rig = MovementRig()
+            self.rig.add(self.camera)
+            self.rig.set_pos([0.5, 1, 5])
 
-        grid.rotate_x(-pi/2)
-        self.scene.add(grid)
+            self.scene.add(self.rig)
+
+        if display_grid:
+            grid = GridTool(size = self.camera.far,division=self.camera.far,grid_color=[1,1,1],center_color=[1,1,0])
+
+            grid.rotate_x(-pi/2)
+            self.scene.add(grid)
         
 
 
     def update(self):
-        self.rig.update(self.input_handler, self.timer.delta_time())
+        if self.rig is not None:
+            self.rig.update(self.input_handler, self.timer.delta_time())
+        
         self.renderer.render(self.scene, self.camera)
