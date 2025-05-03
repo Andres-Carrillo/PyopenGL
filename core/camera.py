4a19c7b832e5a4dac292c8271f3c24d448a5ed1e
andres <andres.carrillo@appliedmedical.com>
2025-05-03 13:07:16 -0700

    adding orthagraphic projection and perspective change

--- core/camera.py
+++ core/camera.py
@@ -16,3 +16,19 @@ class Camera(Object3D):
     def update_view_matrix(self):
         """Update the view matrix based on the camera's position and orientation"""
         self.view_matrix = inv(self.get_world_matrix())
+
+
+    def set_perspective(self,angle_of_view:float = 50.0,aspect_ratio:float=1.0, near:float = 0.1, far:float =1000):
+        """Set the perspective projection matrix"""
+        self.projection_matrix = Matrix.mat4_perspective(angle_of_view,aspect_ratio,near,far)
+        self.angle_of_view = angle_of_view
+        self.aspect_ratio = aspect_ratio
+        self.near = near
+        self.far = far
+
+
+
+
+
+    def set_orthographic(self, left=-1, right=1, bottom=-1, top=1, near=-1, far=1):
+        self.projection_matrix = Matrix.make_orthographic(left, right, bottom, top, near, far)
