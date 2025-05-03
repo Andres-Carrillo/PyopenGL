98bb4af848a11d6851bebc8a3a8241c0a2787af3
andres <andres.carrillo@appliedmedical.com>
2025-05-03 13:08:00 -0700

    implemented look at matrix and orthographic projection

--- core/matrix.py
+++ core/matrix.py
@@ -1,11 +1,13 @@
 import numpy as np
 from math import sin,cos,tan,pi
-
+# from pyglm import glm ## want to use glm but not necessarily right now as performance is currently not an issue
 class Matrix(object):
 
     # Construct a 2D matrix of dimension size x size
     @staticmethod
     def identity(size:int)->np.ndarray:
+        # glm.identity(size)
+        # print(glm.identity(size))
         return np.identity(size,dtype=np.float32)
 
     # Construct 4x4 identity matrix for basis of 3D Transformations
@@ -78,7 +80,8 @@ class Matrix(object):
         mat[2][2] = scale_val
 
         return mat
-    
+        
+    @staticmethod
     def mat4_scale(x:float,y:float,z:float)->np.ndarray:
         mat = Matrix.mat4_identity()
         
@@ -102,4 +105,36 @@ class Matrix(object):
              [0, d, 0, 0],
              [0, 0, b, c],
              [0, 0, -1, 0]]
+        ).astype(np.float32)
+    
+    @staticmethod
+    def make_orthographic(left=-1, right=1, bottom=-1, top=1, near=-1, far=1):
+        return np.array(
+            [[2 / (right - left), 0, 0, -(right + left) / (right - left)],
+             [0, 2 / (top - bottom), 0, -(top + bottom) / (top - bottom)],
+             [0, 0, -2 / (far - near), -(far + near) / (far - near)],
+             [0, 0, 0, 1]]
+        ).astype(float)
+
+    @staticmethod
+    def make_look_at(position, target):
+        world_up = [0, 1, 0]
+        forward = np.subtract(target, position)
+        right = np.cross(forward, world_up)
+        # If forward and world_up vectors are parallel,
+        # the right vector is zero.
+        # Fix this by perturbing the world_up vector a bit
+        if np.linalg.norm(right) < 1e-6:
+            offset = np.array([0, 0, -1e-3])
+            right = np.cross(forward, world_up + offset)
+        up = np.cross(right, forward)
+        # All vectors should have length 1
+        forward = np.divide(forward, np.linalg.norm(forward))
+        right = np.divide(right, np.linalg.norm(right))
+        up = np.divide(up, np.linalg.norm(up))
+        return np.array(
+            [[right[0], up[0], -forward[0], position[0]],
+             [right[1], up[1], -forward[1], position[1]],
+             [right[2], up[2], -forward[2], position[2]],
+             [0, 0, 0, 1]]
         ).astype(float)
