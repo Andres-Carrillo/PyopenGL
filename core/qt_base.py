7485501b2fc015606c2d839e05e75f0cd3a8628d
andres <andres.carrillo@appliedmedical.com>
2025-05-11 21:14:58 -0700

    created base opengl qt app class

--- core/qt_base.py
+++ core/qt_base.py
@@ -99,12 +99,15 @@ from threading import Lock
 from tools.movement_rig import MovementRig
 from tools.grid import GridTool
 from math import pi
+from core.utils.qt_input import Input
 # main GL Context
 class GLWidget(QtOpenGL.QGLWidget):
     def __init__(self,parent:QtWidgets.QWidget=None,resolution:list=[800,600],
-                 background_color:list= [0.0,0.0,0.0],show_fps:bool=True,
-                 display_grid:bool = False,static_camera:bool = False,major_version:int=3,minor_version:int=3):
+                 background_color:list= [0.0,0.0,0.0],show_fps:bool=False,
+                 display_grid:bool = False,static_camera:bool = False,major_version:int=4,minor_version:int=5) -> None:
+        
         super().__init__(parent)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
         
 
         self._fps_counter = FPS()
@@ -119,11 +122,12 @@ class GLWidget(QtOpenGL.QGLWidget):
         self._camera.set_position([0, 0, 2])
         self._scene = Scene()
         self._renderer = Renderer(clear_color=self._background_color,window_width=self._resolution[0],window_height=self._resolution[1])
-        # self.initializeGL()
+        self._input_handler = Input()
+        self._timer = Timer()
+    
         self.rig = None
 
        
-
     @property
     def title(self) -> str:
         return self._title
@@ -145,27 +149,23 @@ class GLWidget(QtOpenGL.QGLWidget):
             self._scene.add(grid)
     
     def paintGL(self):
-        # pass
-         print("paintGL")
-         self._fps_counter.update()
-         self.parent().setWindowTitle(f"{self.parent()._title} - FPS: {self._fps_counter._fps:.2f}")
-         print(f"FPS: {self._fps_counter._fps:.2f}")
-        #  self.setContext
+        self._update()
+
+    def _update(self):
+         # show fps
+        if self._show_fps:
+            self._fps_counter.update()
+            self.parent().setWindowTitle(f"{self.parent()._title} - FPS: {self._fps_counter._fps:.2f}")
+
         # make this the current context
-         self.makeCurrent()
-         # update the input handler
-         if self.rig is not None:
-            self.rig.update(self.input_handler, self.timer.delta_time())
-        #  gl.glClear(gl.GL_COLOR_BUFFER_BIT|gl.GL_DEPTH_BUFFER_BIT)
-         self._renderer.render(self._scene, self._camera)
-        #  gl.glBegin(gl.GL_TRIANGLES)
-        #  gl.glColor3f(1.0, 0.0, 0.0)
-        #  gl.glVertex3f(0.0, 1.0, 0.0)
-        #  gl.glColor3f(0.0, 1.0, 0.0)
-        #  gl.glVertex3f(-1.0, -1.0, 0.0)
-        #  gl.glColor3f(0.0, 0.0, 1.0)
-        #  gl.glVertex3f(1.0, -1.0, 0.0)
-        #  gl.glEnd()
+        self.makeCurrent()
+
+        # if the camera is not static, update the camera position if needed
+        if not self._static_camera:
+            self.rig.update(self._input_handler, self._timer.delta_time())
+
+        # render the scene
+        self._renderer.render(self._scene, self._camera)
 
     def resizeGL(self, w, h):
         # update viewport
@@ -184,32 +184,37 @@ class GLWidget(QtOpenGL.QGLWidget):
     def update_frame(self):
         self.update()
 
+    def keyPressEvent(self, event):
+        self._input_handler.key_press_event(event)
+        if event.key() == QtCore.Qt.Key_Escape:
+            self._input_handler.quit = True
+            self.parent().close()
+
+    def keyReleaseEvent(self, event):
+        """Handle key release events."""
+        self._input_handler.key_release_event(event)
 
 
-class QGLTestWindow(QtWidgets.QMainWindow):
-    def __init__(self,clear_color:list=[0.0, 0.0, 0.0], title:str="Test",display_grid:bool=True,static_camera:bool=False)-> None:
+class QGLApp(QtWidgets.QMainWindow):
+    def __init__(self,clear_color:list=[0.0, 0.0, 0.0], title:str="QGLApp",display_grid:bool=True,static_camera:bool=False,frame_limit:int=180)-> None:
         super().__init__()
-        self.gl_widget = GLWidget(self,background_color=clear_color,display_grid=True,static_camera=False)
+        self.setMinimumSize(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.setMouseTracking(True)
+        self.gl_widget = GLWidget(self,background_color=clear_color,display_grid=display_grid,static_camera=static_camera,show_fps=True)
         self.setCentralWidget(self.gl_widget)
         self._running = True
         self._title = title
+        self._frame_limit = frame_limit
 
         self.setWindowTitle(self._title)
         self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(int((1/self._frame_limit) * 1000)) # convert to milliseconds
         self.timer.timeout.connect(self.update_gl)
 
 
     def run(self):
-        self.timer.start(1) #~60fps
-
+        self.timer.start() #~60fps
 
                 # break
     def update_gl(self):
         self.gl_widget.update()
-
-# if __name__ == "__main__":
-#     import sys
-#     app = QtWidgets.QApplication(sys.argv)
-#     window = GlWidget()
-#     window.show()
-#     sys.exit(app.exec_())
