38143aae9fe3393d6bb4cc958bdc1b9248a9509e
andres <andres.carrillo@appliedmedical.com>
2025-05-16 01:33:20 -0700

    testing framework with imgui included

---
+++ imgui_tests/framework_test.py
@@ -0,0 +1,35 @@
+
+import pathlib
+import sys
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning, module="pygame")
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[1])
+
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+# from core.base import ImGuiBase
+
+from meshes.mesh import Mesh
+from geometry.box import BoxGeometry
+from material.surface import SurfaceMaterial
+# from core.input import Input
+
+# import imgui
+# from material.phong import PhongMaterial
+from core.base import BaseApp
+
+# Run the application
+if __name__ == "__main__":
+    app = BaseApp( width=800, height=600,static_camera=False)
+
+    # adding objects to the scene before the app is run
+    box_geo = BoxGeometry()
+    surface_material = SurfaceMaterial()
+    box = Mesh(geometry=box_geo, material=surface_material)
+    box.set_position([0, 0, 0])
+    app.add_to_scene(box)
+
+    app.run()
