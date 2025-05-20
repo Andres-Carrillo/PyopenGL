856fd4d0a08c16b1b395cd9d627052e1236f3145
andres <andres.carrillo@appliedmedical.com>
2025-05-20 00:34:31 -0700

    created uniform editor for adding new uniforms to materials and fully implemented shader editor.

--- tools/imgui_tools.py
+++ tools/imgui_tools.py
@@ -1,10 +1,12 @@
 import imgui
 from meshes.mesh import Mesh
 import math
+from core.utils.openGLUtils import GlUtils
+from core.glsl.uniform import UNIFORM_TYPE
+from core.glsl.uniform import Uniform
 
 def draw_mesh_uniform_editor(mesh):
-
-    # display the mesh material properties
+    # display the mesh's current uniforms and allow the user to edit them
     imgui.separator()
     open_header, _ = imgui.collapsing_header(mesh.material.material_type + " Uniforms")
     if open_header:
@@ -26,8 +28,8 @@ def draw_mesh_uniform_editor(mesh):
                 if changed:
                     mesh.material.set_properties({"base_color": new_color})
 
-
 def draw_material_settings(mesh):
+    # display the mesh's current material settings and allow the user to edit them
     imgui.separator()
     open_header, _ = imgui.collapsing_header(mesh.material.material_type + " Settings")
     if open_header:
@@ -68,12 +70,36 @@ def draw_rotation_editor(mesh,range =(-math.pi, math.pi)):
         if x_rot_changed or y_rot_changed or z_rot_changed:
             mesh.set_euler_rotation([new_x_rot*360, new_y_rot*360, new_z_rot*360])
 
+def draw_uniform_maker(uniform_name:str = "", uniform_type:int = 0, uniform_data:str = ""):
+    # draw a window to create a new uniform
+    make = False
+    imgui.text("Create a new uniform:")
+    imgui.separator()
+    _,new_uniform_data = imgui.input_text("Uniform Data", uniform_data)
+    _,new_uniform_name = imgui.input_text("Uniform Name", uniform_name)
+    _, new_uniform_type = imgui.combo("Uniform Type", uniform_type, [str(type) for type in UNIFORM_TYPE])
+    if imgui.button("Create"):
+        make = True
+        # self.mesh.material.add_uniform(uniform_name, uniform_type)
+        # print("create a new uniform")
+        # draw_uniform_maker()
+    imgui.separator()
+
+    return new_uniform_name, new_uniform_type, new_uniform_data, make
+
+
 class ShaderEditor:
-    def __init__(self, mesh: Mesh = None,window_width:int = 800, window_height:int = 300):
+    def __init__(self, mesh: Mesh = None,window_width:int = 800, window_height:int = 300,embedded:bool = False):
+        self.embedded = embedded
         self.window_width = window_width
         self.window_height = window_height
         self.show_editor = False
         self.compile_error = False
+        self.edit_uniforms = False
+        self.creating_uniform = False
+        self.temp_uniform_type = 0
+        self.temp_uniform_name = ""
+        self.temp_uniform_data = ""
         self.mesh = mesh
         self.menu_bbox = [0, 0, 0, 0]
         self.error_message = ""
@@ -97,8 +123,40 @@ class ShaderEditor:
 
     def show(self):
         if self.show_editor:
-            imgui.begin("Shader Editor", imgui.WINDOW_ALWAYS_AUTO_RESIZE)
+            
             imgui.separator()
+            if self.edit_uniforms:
+                imgui.text("Edit Uniforms:")
+                imgui.separator()
+                draw_mesh_uniform_editor(self.mesh)
+                imgui.separator()
+
+                
+                # handle adding a new uniform to the mesh material
+                if imgui.button("Add a new Uniform"):
+                    self.creating_uniform = True
+
+                if self.creating_uniform:
+                    self.temp_uniform_name,self.temp_uniform_type, self.temp_uniform_data,add_uniform = draw_uniform_maker(uniform_name=self.temp_uniform_name, uniform_type=self.temp_uniform_type, uniform_data=self.temp_uniform_data)
+                
+                    if add_uniform:
+                        uniform_data =  GlUtils.make_uniform_data( UNIFORM_TYPE(self.temp_uniform_type), self.temp_uniform_data)
+                        self.mesh.material.add_uniform(self.temp_uniform_name,  uniform_data,str(UNIFORM_TYPE(self.temp_uniform_type)))
+                        self.mesh.material.locate_uniforms()    
+                        
+                        # reset the buffers
+                        self.creating_uniform = False
+                        self.temp_uniform_name = ""
+                        self.temp_uniform_data = ""
+                        self.temp_uniform_type = 0
+
+                imgui.separator()
+                # imgui.same_line()
+                if imgui.button("Done"):
+                    self.edit_uniforms = False
+
+
+
             imgui.text("Vertex Shader Editor:")
             imgui.separator()
             _, self.vertex_shader = imgui.input_text_multiline("", self.vertex_shader, 1024 * 16, self.window_width, self.window_height)
@@ -122,14 +180,14 @@ class ShaderEditor:
 
             imgui.same_line()   
             if(imgui.button("Edit Uniforms")):
-                print("Edit Uniforms")
+                self.edit_uniforms = not self.edit_uniforms
+                # print("Edit Uniforms")
 
             window_pos = imgui.get_window_position()
             window_size = imgui.get_window_size()
             self.menu_bbox = [window_pos[0], window_pos[1], window_pos[0] + window_size[0], window_pos[1] + window_size[1]]
-
-            imgui.end()
-
+    
+    
     def compile_shaders(self):
         try:
             self.mesh.material.compile_shaders(self.vertex_shader, self.fragment_shader)
@@ -147,7 +205,7 @@ class ShaderEditor:
 class MeshEditor:
     def __init__(self, mesh: Mesh = None):
         self.mesh = mesh
-        self.shader_editor = ShaderEditor(mesh)
+        self.shader_editor = ShaderEditor(mesh,embedded=True)
         self.menu_bbox = [0, 0, 0, 0]
 
     def change_mesh(self, mesh: Mesh = None):
