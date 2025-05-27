65fb554a841593c963bd5c244992d0611549f976
andres <andres.carrillo@appliedmedical.com>
2025-05-27 01:45:46 -0700

    simple physics engine to handle collision detection.

---
+++ core/physics/engine.py
@@ -0,0 +1,25 @@
+
+class PhysicsEngine:
+    def __init__(self,forces:list = [[0, -9.81, 0]]):
+        self.forces = forces
+        self.objects = []
+
+    def add_object(self, obj):
+        self.objects.append(obj)
+
+    def remove_object(self, obj):
+        self.objects.remove(obj)
+
+    def update(self, delta_time):
+        for obj in self.objects:
+            obj.update(delta_time, self.gravity)
+
+
+    def handle_collisions(self):
+        for i in range(len(self.objects)):
+            for j in range(i + 1, len(self.objects)):
+                obj_a = self.objects[i]
+                obj_b = self.objects[j]
+                if obj_a.collides_with(obj_b):
+                    obj_a.handle_collision(obj_b)
+                    obj_b.handle_collision(obj_a)
