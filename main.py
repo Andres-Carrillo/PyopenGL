8f5121dfa1bb8a706a1dc25678ad5595430c5094
andres <andres.carrillo@appliedmedical.com>
2025-06-26 00:44:34 -0700

    update to main to showcase base scene editor

--- main.py
+++ main.py
@@ -7,7 +7,10 @@ from core.utils.openGLUtils import GlUtils
 from core.base import Base
 
 
+from core.base import SceneEditor
+        
+# Run the application
 if __name__ == "__main__":
-    my_app = Base()
-    my_app.run()
-    my_app.quit()
+
+    app = SceneEditor( width=1280, height=720)
+    app.run()
