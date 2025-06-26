200036a9483a681558b67b92f19a10a51a9a705e
andres <andres.carrillo@appliedmedical.com>
2025-06-26 00:23:24 -0700

    began testing glm based matrix math

--- core/utils/matrix.py
+++ core/utils/matrix.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyglm as glm
 from math import sin,cos,tan,pi
 # from pyglm import glm ## want to use glm but not necessarily right now as performance is currently not an issue
 class Matrix(object):
@@ -143,3 +144,100 @@ class Matrix(object):
 
 
 
+class GLMMatrix(object):
+    """ A wrapper around pyglm to provide a more Pythonic interface.
+        Rotations should be quaternions, not Euler angles. But should take Euler angles for compatibility.
+    """
+    @staticmethod
+    def mat4_identity():
+        return glm.mat4(1.0)
+
+    @staticmethod
+    def mat4_translation(x, y, z):
+        return glm.translate(glm.mat4(1.0), glm.vec3(x, y, z))
+
+    @staticmethod
+    def mat4_rotate_x(angle):
+        return glm.rotate(glm.mat4(1.0), angle, glm.vec3(1, 0, 0))
+
+    @staticmethod
+    def mat4_rotate_y(angle):
+        return glm.rotate(glm.mat4(1.0), angle, glm.vec3(0, 1, 0))
+
+    @staticmethod
+    def mat4_rotate_z(angle):
+        return glm.rotate(glm.mat4(1.0), angle, glm.vec3(0, 0, 1))
+
+    @staticmethod
+    def mat4_scale(scale_val):
+        return glm.scale(glm.mat4(1.0), glm.vec3(scale_val, scale_val, scale_val))
+    @staticmethod
+    def mat4_scale(x, y, z):
+        return glm.scale(glm.mat4(1.0), glm.vec3(x, y, z))
+    @staticmethod
+    def mat4_perspective(angle_of_view=60.0, aspect_ratio=1.0, near=0.1, far=1000):
+        return glm.perspective(glm.radians(angle_of_view), aspect_ratio, near, far)
+    @staticmethod
+    def make_orthographic(left=-1, right=1, bottom=-1, top=1, near=-1, far=1):
+        return glm.ortho(left, right, bottom, top, near, far)
+   
+    @staticmethod
+    def mat4_to_np(mat):
+        """ Convert a glm.mat4 to a numpy array. """
+        return np.array(mat).astype(float)
+    @staticmethod
+    def np_to_mat4(np_array):
+        """ Convert a numpy array to a glm.mat4. """
+        return glm.mat4(*np_array.flatten().tolist())
+    @staticmethod
+    def mat4_multiply(mat_a, mat_b):
+        """ Multiply two glm.mat4 matrices. """
+        return glm.mat4(mat_a) * glm.mat4(mat_b)
+    @staticmethod
+    def mat4_inverse(mat):
+        """ Inverse of a glm.mat4 matrix. """
+        return glm.inverse(mat)
+    @staticmethod
+    def mat4_transpose(mat):
+        """ Transpose of a glm.mat4 matrix. """
+        return glm.transpose(mat)
+    @staticmethod
+    def mat4_determinant(mat):
+        """ Determinant of a glm.mat4 matrix. """
+        return glm.determinant(mat)
+    
+    @staticmethod
+    def mat4_decompose(mat):
+        """ Decompose a glm.mat4 matrix into translation, rotation, and scale. """
+        translation, rotation, scale = glm.decompose(mat)
+        return translation, rotation, scale
+    
+    @staticmethod
+    def mat4_look_at(position, target, up=glm.vec3(0, 1, 0)):
+        """ Create a look-at matrix using glm. """
+        return glm.lookAt(glm.vec3(position), glm.vec3(target), glm.vec3(up))
+    
+     
+    @staticmethod
+    def make_look_at(position, target):
+        world_up = glm.vec3(0, 1, 0)
+
+        forward = glm.normalize(glm.vec3(target) - glm.vec3(position))
+        right = glm.normalize(glm.cross(forward, world_up))
+        up = glm.normalize(glm.cross(right, forward))
+        
+        return glm.mat4(
+            right.x, up.x, -forward.x, position[0],
+            right.y, up.y, -forward.y, position[1],
+            right.z, up.z, -forward.z, position[2],
+            0, 0, 0, 1
+        )
+    
+    @staticmethod
+    def mat4_frustum(left, right, bottom, top, near, far):
+        """ Create a frustum matrix using glm. """
+        return glm.frustum(left, right, bottom, top, near, far)
+    @staticmethod
+    def mat4_perspective_fov(fov, aspect, near, far):
+        """ Create a perspective projection matrix using glm. """
+        return glm.perspective(glm.radians(fov), aspect, near, far)
