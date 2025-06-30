297e85e51196600b6a0ca233a58643b0a690fae5
andres <andres.carrillo@appliedmedical.com>
2025-06-30 00:59:04 -0700

    testing terrain generation

--- imgui_tests/framework_test.py
+++ imgui_tests/framework_test.py
@@ -17,19 +17,26 @@ from geometry.simple3D.box import BoxGeometry
 from material.basic.surface import SurfaceMaterial
 # from core.input import Input
 
+from meshes.terrain import Terrain
+
 # import imgui
 # from material.phong import PhongMaterial
 from core.base import BaseApp
 
 # Run the application
 if __name__ == "__main__":
-    app = BaseApp( width=800, height=600,static_camera=False)
+    app = BaseApp( width=800, height=600,static_camera=False,display_grid=False)
 
+    
+    landscape = Terrain()
     # adding objects to the scene before the app is run
     box_geo = BoxGeometry()
     surface_material = SurfaceMaterial()
     box = Mesh(geometry=box_geo, material=surface_material)
     box.set_position([0, 0, 0])
-    app.add_to_scene(box)
+    # app.add_to_scene(box)
+
+    app.add_to_scene(landscape)
+
 
     app.run()
