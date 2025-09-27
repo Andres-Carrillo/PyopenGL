a9258d482f023e945b7e97b1f75184bebaa31dc3
andres <andres.carrillo@appliedmedical.com>
2025-09-26 23:44:05 -0700

    creating base entity class for entity component system based physics engine.

---
+++ core/entity.py
@@ -0,0 +1,59 @@
+from object3D import Object3D
+import enum
+
+class Components(enum.IntFlag):
+    NONE = 0
+    RIGIDBODY = 1 << 0
+    COLLIDER = 1 << 1
+    MESHRENDERER = 1 << 2
+    CAMERA = 1 << 3
+    LIGHT = 1 << 4
+
+class Entity:
+    def __init__(self,object3d:Object3D=None):
+        self.object3d = object3d if object3d is not None else Object3D()
+        self.components = Components.NONE
+        self.comp_data = {}
+
+    def add_component(self,component:Components,data=None):
+        self.components |= component
+        if data is not None:
+            self.comp_data[component] = data
+    
+
+    def remove_component(self,component:Components):
+        self.components &= ~component
+        if component in self.comp_data:
+            del self.comp_data[component]
+    
+    def get_component_data(self,component:Components):
+        return self.comp_data.get(component,None)
+    
+    def has_component(self,component:Components):
+        return (self.components & component) == component
+    
+    def toggle_component(self,component:Components):
+        self.components ^= component
+        
+    def get_components(self):
+        return [comp for comp in Components if comp != Components.NONE and self.has_component(comp)]
+    
+    def __str__(self):
+        return f"Entity(components={[str(comp) for comp in self.get_components()]})"
+    
+
+
+def entity_spawner(Object3D:Object3D=None,components:Components=Components.NONE,comp_data:dict=None):
+    entity = Entity(Object3D)
+    entity.components = components
+
+    if comp_data is not None:
+        entity.comp_data = comp_data
+
+    return entity
+
+
+def add_collider(entity:Entity,collision_type:Components,data:dict=None):
+    entity.add_component(Components.COLLIDER,data={'collision_type':collision_type,'data':data})
+
+
