b36e036c9d754cd1e8545f181dcd8d3a25fc7109
andres <andres.carrillo@appliedmedical.com>
2025-10-01 19:52:14 -0700

    adding add_force function

--- core/components/physics.py
+++ core/components/physics.py
@@ -1,6 +1,10 @@
 class PhysicsComponent:
     def __init__(self, mass=1.0, velocity=[0,0,0], acceleration=[0,0,0], forces=None):
         self.mass = mass
+        self.local_forces = forces or []
         self.velocity = velocity
         self.acceleration = acceleration
-        self.forces = forces or []
+
+
+    def add_force(self, force:list = [0,0,0]):
+        self.local_forces.append(force)
