5656e29b61337ec5307bf87fb8aec1f943392f92
andres <andres.carrillo@appliedmedical.com>
2025-10-15 01:10:05 -0700

    Rename for simplification

--- core/components/types.py
+++ core/components/types.py
@@ -4,7 +4,7 @@ class Components(enum.IntFlag):
     NONE = 0 # No components
     RIGIDBODY = 1 << 0 # Rigidbody component
     COLLIDER = 1 << 1 # Collider component
-    MESHRENDERER = 1 << 2 # MeshRenderer component
+    MESH = 1 << 2 # Mesh component
     CAMERA = 1 << 3 # Camera component
     LIGHT = 1 << 4 # Light component
     MOVEABLE = 1 << 5 # Component that allows movement
