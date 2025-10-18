00695b580372f57164ac491d78bfc20b7f4f772a
andres <andres.carrillo@appliedmedical.com>
2025-10-18 00:19:50 -0700

    test for new model view controller based apps.

---
+++ tests/mvc_tests/base_app_test.py
@@ -0,0 +1,18 @@
+import pathlib
+import sys
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning, module="pygame")
+# Get the package directory
+package_dir = str(pathlib.Path(__file__).resolve().parents[2])
+
+# Add the package directory into sys.path if necessary
+if package_dir not in sys.path:
+    sys.path.insert(0, package_dir)
+
+from apps.base import MVCApp
+from apps.scene_editor import SceneEditorMVCApp
+
+app = SceneEditorMVCApp()
+
+if __name__ == "__main__":
+    app.run()
